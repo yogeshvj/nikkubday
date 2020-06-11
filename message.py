7cf9319347f04a10f0a94a47d8e3b5364952835f
yogeshvj <yogeshuvijay@gmail.com>
2020-06-11 22:05:50 +0530

    Create message.py

---
+++ message.py
@@ -0,0 +1,8 @@
+message = ' !!! ayitoG yadB yppaH\n'[::-1]
+message = message + '.atkas oh ihan ihb ey erak an esarabme okpa ot oh raga rua ,atkas ihan ot oh asia ,oh an tfig iok ep yadb ekpA\n'[::-1]
+message = message + '.agedap anhdnuhd esu okpa ik iah ey tab sab,iah apuhc ih rap roolf ts1 iam rahg tfig akpA\n'[::-1]
+message = message + '.radna egneyap aj ik ihan atgal es rud rap ,radna agedap anaj ot iah anel namaS\n'[::-1]
+message = message + '.nredrom ifak iah oj hagaj isia oh etkar ,nasrahd eksi rab ke oh etrak elhp es enaj iam idahS\n'[::-1]
+message = message + '.akpa iah nuyk atataf edoc ik egneyaj hjamas ,anel hcup ekrak enohp ot elim an ihb ba ,iah ifak tnih ih antI\n'[::-1]
+message = message + '\n): bawan etohc yadB yppaH'[::-1]
+print(message)
